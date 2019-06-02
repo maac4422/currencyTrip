538821492be08ef565677b53140c0e91f483d12e
Guillermo costa <guillermo.costa@rappi.com>
2019-06-02 14:49:46 -0500

    Created main views

--- currency trip/CurrenciesViewController.swift
+++ currency trip/CurrenciesViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class CurrenciesViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
