06dca42c33498486181355a3786d2fe6aa1df64c
Guillermo costa <guillermo.costa@rappi.com>
2019-06-02 14:06:39 -0500

    Initial Commit

---
+++ currency trip/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  currency trip
+//
+//  Created by Guillermo Costa on 6/2/19.
+//  Copyright © 2019 Guillermo Costa. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ currency trip/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  currency trip
+//
+//  Created by Guillermo Costa on 6/2/19.
+//  Copyright © 2019 Guillermo Costa. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
